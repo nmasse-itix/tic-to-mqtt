a054b4c5466b03443a4a6af034e55f570aeaf34e
Nicolas MASSE <nicolas.masse@itix.fr>
2022-01-26 16:12:26 +0100

    initial commit

---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+#include "tic.h"
+
+void app_main(void) {
+    tic_uart_init();
+}
